bef9b92838e1eaed9b1cae56574d9e2ab9a03cbd
Silbia <silbiaislam.29@gmail.com>
2020-11-27 14:15:14 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Component } from 'react';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Header from "../../netart_webpage/src/Components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+console.log(logo);
+
+function Header() {
+    return <img src={logo} alt="Logo" />;
+}
+
+export default Header;
